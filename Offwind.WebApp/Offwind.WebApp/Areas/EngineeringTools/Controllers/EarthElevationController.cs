e40b025f5e55bbfc0f7807e361df8198aaf7d4d4
Vlad <vlad.ogay@nrg-soft.com>
2012-12-04 19:27:52 +0400

    EarthElevation

--- Offwind.WebApp/Offwind.WebApp/Areas/EngineeringTools/Controllers/EarthElevationController.cs
+++ Offwind.WebApp/Offwind.WebApp/Areas/EngineeringTools/Controllers/EarthElevationController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading;
+using System.Web.Mvc;
 
 namespace MvcApplication1.Areas.EngineeringTools.Controllers
 {
@@ -6,11 +7,20 @@ namespace MvcApplication1.Areas.EngineeringTools.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Title = "World Map | STL Earth Elevation | Offwind";
             return View();
+        }
+
+        public FileResult Generate()
+        {
+            ViewBag.Title = "Generate | STL Earth Elevation | Offwind";
+            Thread.Sleep(3000);
+            return File(new byte[0], "text/plain", "result.stl");
         }
 
         public ActionResult Results()
         {
+            ViewBag.Title = "Results | STL Earth Elevation | Offwind";
             return View();
         }
     }

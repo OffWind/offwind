295807d69cf999900a54c9273196544f44a50661
Vlad <vlad.ogay@nrg-soft.com>
2012-12-08 21:48:57 +0400

    Some visual improvements on logon form

--- Offwind.WebApp/Offwind.WebApp/Infrastructure/Helper.cs
+++ Offwind.WebApp/Offwind.WebApp/Infrastructure/Helper.cs
@@ -1,5 +1,7 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Html;
 
 namespace Offwind.WebApp.Infrastructure
 {
@@ -17,6 +19,22 @@ namespace Offwind.WebApp.Infrastructure
                 return page.Html.Raw("class=\"active\"");
             }
             return page.Html.Raw("");
+        }
+
+        public static IHtmlString BootstrapValidationSummary(this HtmlHelper html)
+        {
+            string retVal = "";
+            if (html.ViewData.ModelState.IsValid)
+                return html.Raw("");
+            retVal += "<div class='alert alert-error'>";
+            retVal += "<ul>";
+            foreach (var key in html.ViewData.ModelState.Keys)
+            {
+                foreach (var err in html.ViewData.ModelState[key].Errors)
+                    retVal += "<li>" + html.Encode(err.ErrorMessage) + "</li>";
+            }
+            retVal += "</ul></div>";
+            return html.Raw(retVal);
         }
     }
 }

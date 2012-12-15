d81a058054fbacbaa8b65b7c3503d56f4ec2e792
Vlad <vlad.ogay@nrg-soft.com>
2012-12-16 03:31:44 +0400

    VFieldR.cs

--- Offwind.WebApp/Offwind.WebApp/Areas/CFD/Models/BoundaryConditions/VFieldR.cs
+++ Offwind.WebApp/Offwind.WebApp/Areas/CFD/Models/BoundaryConditions/VFieldR.cs
@@ -1,38 +1,47 @@
-﻿using Offwind.Products.OpenFoam.Models.Fields;
+﻿using System.ComponentModel;
+using Offwind.Products.OpenFoam.Models.Fields;
 
 namespace Offwind.WebApp.Areas.CFD.Models.BoundaryConditions
 {
     public class VFieldR
     {
-        public decimal InternalField1 { get; set; }
-        public decimal InternalField2 { get; set; }
-        public decimal InternalField3 { get; set; }
-        public decimal InternalField4 { get; set; }
-        public decimal InternalField5 { get; set; }
-        public decimal InternalField6 { get; set; }
+        public VFieldVectorValue2 InternalField { get; set; }
 
+        [DisplayName("type")]
+        [ReadOnly(true)]
         public PatchType BottomType { get; set; }
 
+        [DisplayName("type")]
+        [ReadOnly(true)]
         public PatchType TopType { get; set; }
 
+        [DisplayName("type")]
+        [ReadOnly(true)]
         public PatchType WestType { get; set; }
-        public decimal WestValue1 { get; set; }
-        public decimal WestValue2 { get; set; }
-        public decimal WestValue3 { get; set; }
-        public decimal WestValue4 { get; set; }
-        public decimal WestValue5 { get; set; }
-        public decimal WestValue6 { get; set; }
 
+        [DisplayName("value")]
+        public VFieldVectorValue2 WestValue { get; set; }
+
+        [DisplayName("type")]
+        [ReadOnly(true)]
         public PatchType EastType { get; set; }
 
+        [DisplayName("type")]
+        [ReadOnly(true)]
         public PatchType NorthType { get; set; }
 
+        [DisplayName("type")]
+        [ReadOnly(true)]
         public PatchType SouthType { get; set; }
-        public decimal SouthValue1 { get; set; }
-        public decimal SouthValue2 { get; set; }
-        public decimal SouthValue3 { get; set; }
-        public decimal SouthValue4 { get; set; }
-        public decimal SouthValue5 { get; set; }
-        public decimal SouthValue6 { get; set; }
+
+        [DisplayName("value")]
+        public VFieldVectorValue2 SouthValue { get; set; }
+
+        public VFieldR()
+        {
+            InternalField = new VFieldVectorValue2();
+            WestValue = new VFieldVectorValue2();
+            SouthValue = new VFieldVectorValue2();
+        }
     }
 }
--- Offwind.WebApp/Offwind.WebApp/Areas/CFD/Models/BoundaryConditions/VFieldVectorValue2.cs
+++ Offwind.WebApp/Offwind.WebApp/Areas/CFD/Models/BoundaryConditions/VFieldVectorValue2.cs
@@ -2,11 +2,14 @@
 
 namespace Offwind.WebApp.Areas.CFD.Models.BoundaryConditions
 {
-    public class VFieldVectorValue
+    public class VFieldVectorValue2
     {
         public PatchValueType Type { get; set; }
         public decimal Value1 { get; set; }
         public decimal Value2 { get; set; }
         public decimal Value3 { get; set; }
+        public decimal Value4 { get; set; }
+        public decimal Value5 { get; set; }
+        public decimal Value6 { get; set; }
     }
 }

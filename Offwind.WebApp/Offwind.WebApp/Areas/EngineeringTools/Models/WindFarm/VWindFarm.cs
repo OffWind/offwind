7e5203dae431f39f4abc65563639156c50c51c06
vkolesnikov <vkolesnikov@mail333.com>
2013-03-16 00:23:49 +0400

    engineering tools -> windfarm sim added

---
+++ Offwind.WebApp/Offwind.WebApp/Areas/EngineeringTools/Models/WindFarm/VWindFarm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Offwind.WebApp.Areas.EngineeringTools.Models.WindFarm
+{
+    public sealed class VWindFarm
+    {
+        public Double StartTime { set; get; }
+        public Double StopTime { set; get; }
+        public Double TimeStep { set; get; }
+        [DisplayName("Wind farm scale")]
+        public Double Scale { set; get; }
+
+        public List<VWindTurbine> Turbines { set; get; }
+
+        public VWindFarm()
+        {
+            StartTime = 0;
+            StopTime = 1000;
+            TimeStep = 0.01;
+            Scale = 5000000;
+            Turbines = new List<VWindTurbine>();
+        }
+    }
+}
---
+++ Offwind.WebApp/Offwind.WebApp/Areas/EngineeringTools/Models/WindFarm/VWindTurbine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+namespace Offwind.WebApp.Areas.EngineeringTools.Models.WindFarm
+{
+    public sealed class VWindTurbine
+    {
+        public Double rho { set; get; }
+        public Double radius { set; get; }
+        public Double rated { set; get; }
+        public Double Cp { set; get; }
+        public Double speed { set; get; }
+
+        public VWindTurbine()
+        {
+            
+        }
+
+        public VWindTurbine(string[] array)
+        {
+            rho = Convert.ToDouble(array[0]);
+            radius = Convert.ToDouble(array[1]);
+            rated = Convert.ToDouble(array[2]);
+            Cp = Convert.ToDouble(array[3]);
+            speed = Convert.ToDouble(array[4]);
+        }
+    }
+}

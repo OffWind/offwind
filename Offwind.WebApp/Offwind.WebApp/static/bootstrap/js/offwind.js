3e5ef2cd1a47f5203445413cd00609f5e0db5603
Vlad <vlad.ogay@nrg-soft.com>
2013-04-01 13:40:27 +0400

    offwind.js

---
+++ Offwind.WebApp/Offwind.WebApp/static/bootstrap/js/offwind.js
@@ -0,0 +1,20 @@
+var Utils = { };
+
+Utils.GetWindowSize = function () {
+    var myWidth = 0, myHeight = 0;
+    if (typeof (window.innerWidth) == 'number') {
+        //Non-IE
+        myWidth = window.innerWidth;
+        myHeight = window.innerHeight;
+    } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+        //IE 6+ in 'standards compliant mode'
+        myWidth = document.documentElement.clientWidth;
+        myHeight = document.documentElement.clientHeight;
+    } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+        //IE 4 compatible
+        myWidth = document.body.clientWidth;
+        myHeight = document.body.clientHeight;
+    }
+
+    return { width: myWidth, height: myHeight };
+}

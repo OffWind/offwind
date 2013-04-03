1eea5f3355b9fa80e949270b6b74e8dd71a70340
vkolesnikov <vkolesnikov@mail333.com>
2013-04-04 02:18:45 +0400

    MesoWind UI refactoring. (it was not completed yet)

---
+++ Offwind.WebApp/Offwind.WebApp/Areas/EngineeringTools/Models/MesoWind/VMesoWind.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using Offwind.WebApp.Models;
+
+namespace Offwind.WebApp.Areas.EngineeringTools.Models.MesoWind
+{
+    public class VMesoWind
+    {
+        public DbType DbType { set; get; }
+        public int TotalCount { set; get; }
+        public int FnlCount { set; get; }
+        public int MerraCount { set; get; }
+
+        public bool UseSearchResults { set; get; }
+        public List<SmallMesoscaleTabFile> InterestingPoints { set; get; }
+        public List<MesoscaleTabFile> ImportedPoints { set; get; }
+
+        public VMesoWind()
+        {
+            DbType = DbType.All;
+            UseSearchResults = false;
+            InterestingPoints = new List<SmallMesoscaleTabFile>();
+            ImportedPoints = new List<MesoscaleTabFile>();
+        }
+    }
+}

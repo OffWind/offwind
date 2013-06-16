3533726e63e1b711fac562ba6468a893b978eba2
vkolesnikov <vkolesnikov@mail333.com>
2013-06-17 02:00:48 +0400

    Users management works

--- Offwind.WebApp/Offwind.WebApp/Models/Account/SystemRole.cs
+++ Offwind.WebApp/Offwind.WebApp/Models/Account/SystemRole.cs
@@ -3,11 +3,16 @@
     public class SystemRole
     {
         public const string Admin = "Admin";
+        public const string Partner = "Partner";
         public const string RegularUser = "RegularUser";
+        public const string User = "User";
     }
 
     public enum SystemRoleType
     {
-        Admin, RegularUser
+        Admin,
+        Partner,
+        RegularUser,
+        User
     }
 }

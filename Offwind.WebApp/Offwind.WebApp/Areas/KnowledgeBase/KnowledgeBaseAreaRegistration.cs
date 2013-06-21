0ab35839ac61af2d772a7dd2fa67638944736a32
Vlad <vlad.ogay@nrg-soft.com>
2013-06-21 15:30:38 +0400

    Added KnowledgeBase Changed home page layout

---
+++ Offwind.WebApp/Offwind.WebApp/Areas/KnowledgeBase/KnowledgeBaseAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Offwind.WebApp.Areas.KnowledgeBase
+{
+    public class KnowledgeBaseAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "KnowledgeBase";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "KnowledgeBase_default",
+                "kb/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

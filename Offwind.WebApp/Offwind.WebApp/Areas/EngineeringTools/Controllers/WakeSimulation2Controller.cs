ad2bca8af0f0bfec74e995534f3adc25b9315e5d
victor <vkolesnikov@mail333.com>
2013-08-06 00:58:26 +0400

    Wake Simulator II sceleton

---
+++ Offwind.WebApp/Offwind.WebApp/Areas/EngineeringTools/Controllers/WakeSimulation2Controller.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmitMapper;
+using Offwind.WebApp.Areas.EngineeringTools.Models.WakeSimulation2;
+using Offwind.WebApp.Areas.WindFarms.Models;
+
+namespace Offwind.WebApp.Areas.EngineeringTools.Controllers
+{
+    public class WakeSimulation2Controller : _BaseController
+    {
+        private static VGeneralProperties _model = null;
+        private static List<string> _wfl = null;
+
+        public ActionResult Index()
+        {
+            if (_model == null)
+            {
+                _model = new VGeneralProperties();
+                _wfl = new List<string>();
+            }
+            var model = new VGeneralProperties();
+            ObjectMapperManager.DefaultInstance.GetMapper<VGeneralProperties, VGeneralProperties>().Map(_model, model);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult Save(VGeneralProperties model)
+        {
+            lock(_model)
+            {
+                model.WindFarm = _model.WindFarm;
+                ObjectMapperManager.DefaultInstance.GetMapper<VGeneralProperties, VGeneralProperties>().Map(model, _model);
+            }
+            return View(_model);
+        }
+
+        public JsonResult GetAvailWinFarms()
+        {
+            _wfl.Clear();
+            _wfl.AddRange(_ctx.DWindFarms.Select(entry => entry.Name));
+            if (_model.WindFarm != "")
+            {
+                var index = 0;
+                foreach (var v in _wfl)
+                {
+                    if (v == _model.WindFarm)
+                    {
+                        _wfl.Add(index.ToString(CultureInfo.InvariantCulture));
+                        break;
+                    }
+                    index++;
+                }
+            }
+            return Json(_wfl, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult WindFarmSelected(int id)
+        {
+            lock(_model)
+            {
+                _model.WindFarm = _wfl[id];
+            }
+            return Json("OK");
+        }
+
+        public ActionResult WindFarmInfo(Guid? id)
+        {
+            var dWindFarm = _ctx.DWindFarms.First(e => _model.WindFarm == e.Name);
+            return RedirectToAction("Details", "WindFarm", new { area = "WindFarms", id = dWindFarm.Id, returnTo = "WakeSimulator2" });
+        }
+    }
+}
+
---
+++ Offwind.WebApp/Offwind.WebApp/Areas/EngineeringTools/Models/WakeSimulation2/VGeneralProperties.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using Offwind.WebApp.Models;
+
+namespace Offwind.WebApp.Areas.EngineeringTools.Models.WakeSimulation2
+{
+    public class VGeneralProperties : VWebPage
+    {
+        public decimal StartTime { set; get; }
+        public decimal StopTime { set; get; }
+        public Double TimeStep { set; get; }
+
+        [DisplayName("Air density")]
+        public Double Rho { set; get; }
+
+        public string WindFarm { set; get; }
+
+        public VGeneralProperties()
+        {
+            StartTime = 0;
+            StopTime = 100;
+            TimeStep = 0.125;
+            Rho = 1.53;
+            WindFarm = "";
+        }
+    }
+}

8cbee480905b4c419cc9f7424c0bac0a34429fb8
Vlad <vlad.ogay@nrg-soft.com>
2013-09-11 20:43:39 +0400

    Content

--- Offwind.WebApp/Offwind.WebApp/App_Start/RouteConfig.cs
+++ Offwind.WebApp/Offwind.WebApp/App_Start/RouteConfig.cs
@@ -1,5 +1,9 @@
 ﻿using System.Web.Mvc;
+using System.Linq;
 using System.Web.Routing;
+using Offwind.Web.Core;
+using Offwind.WebApp.Areas.ControlPanel.Tools;
+using Offwind.WebApp.Models;
 
 namespace Offwind.WebApp.App_Start
 {
@@ -8,6 +12,22 @@ namespace Offwind.WebApp.App_Start
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            using (var ctx = new OffwindEntities())
+            {
+                var ct = ContentType.Page.S();
+                foreach (var dc in ctx.DContents.Where(c => c.TypeId == ct))
+                {
+                    if (dc.Route.Trim().Length <= 0) continue;
+                    routes.MapRoute(
+                        dc.Id.ToString(),
+                        dc.Route,
+                        new { controller = "Content", action = "ShowById", id = dc.Id },
+                        new[] { "Offwind.WebApp.Controllers" }
+                    );
+                }
+            }
 
             routes.MapRoute(
                 name: "Default",
---
+++ Offwind.WebApp/Offwind.WebApp/Controllers/ContentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Offwind.Web.Core;
+
+namespace Offwind.WebApp.Controllers
+{
+    public class ContentController : BaseController
+    {
+        public ActionResult ShowById(string id)
+        {
+            Guid guid;
+            DContent dPage;
+            dPage = _ctx.DContents.FirstOrDefault(p => p.Route == id);
+            if (dPage == null && Guid.TryParse(id, out guid))
+            {
+                dPage = _ctx.DContents.First(p => p.Id == guid);
+            }
+            if (dPage == null) return RedirectToAction("Error404", "Home");
+
+            if (!String.IsNullOrWhiteSpace(dPage.BrowserTitle))
+            {
+                ViewBag.TitleSuffix = dPage.BrowserTitle;
+            }
+            else
+            {
+                ViewBag.TitleSuffix = dPage.Title;
+            }
+
+            if (!String.IsNullOrWhiteSpace(dPage.MetaDescription))
+            {
+                ViewBag.MetaDescription = dPage.MetaDescription;
+            }
+
+            return View("Show", dPage);
+        }
+
+    }
+}

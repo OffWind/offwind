adbe2af8a22b675b1b09b19fb883bdee15eb1232
allchk-nrg <alexey.lychkin@nrg-soft.com>
2014-12-25 11:13:05 +0300

    Refactored WCFR code - reordered fields in files data structs

--- Offwind.WakeFarmControl/WakeFarmControlRasmus/WtMatFileDataStructure.cs
+++ Offwind.WakeFarmControl/WakeFarmControlRasmus/WtMatFileDataStructure.cs
@@ -21,23 +21,23 @@ namespace WakeFarmControlR
 
         public class Ctrl
         {
+            public class Pitch
+            {
+                public double Pgain;
+                public double Igain;
+                public double ratelim;
+                public double ulim;
+                public double llim;
+            }
+
             public class Torq
             {
                 public double ratelim;
             }
 
-            public class Pitch
-            {
-                public double Igain;
-                public double Pgain;
-                public double ulim;
-                public double llim;
-                public double ratelim;
-            }
-
-            public double p_rated;
-            public Torq torq = new Torq();
             public Pitch pitch = new Pitch();
+            public Torq torq = new Torq();
+            public double p_rated;
         }
 
         public C_ cp = new C_();
@@ -52,22 +52,22 @@ namespace WakeFarmControlR
 
         public WtMatFileDataStructure(ILMatFile ilMatFile)
         {
-            this.rotor.radius = (double)(ilMatFile.GetArray<double>("wt_rotor_radius"));
-            this.rotor.inertia = (double)(ilMatFile.GetArray<double>("wt_rotor_inertia"));
-            this.rotor.ratedspeed = (double)(ilMatFile.GetArray<double>("wt_rotor_ratedspeed"));
-            this.ctrl.p_rated = (double)(ilMatFile.GetArray<double>("wt_ctrl_p_rated"));
-            this.ctrl.torq.ratelim = (double)(ilMatFile.GetArray<double>("wt_ctrl_torq_ratelim"));
-            this.ctrl.pitch.Igain = (double)(ilMatFile.GetArray<double>("wt_ctrl_pitch_Igain"));
-            this.ctrl.pitch.Pgain = (double)(ilMatFile.GetArray<double>("wt_ctrl_pitch_Pgain"));
-            this.ctrl.pitch.ulim = (double)(ilMatFile.GetArray<double>("wt_ctrl_pitch_ulim"));
-            this.ctrl.pitch.llim = (double)(ilMatFile.GetArray<double>("wt_ctrl_pitch_llim"));
-            this.ctrl.pitch.ratelim = (double)(ilMatFile.GetArray<double>("wt_ctrl_pitch_ratelim"));
-            this.cp.table = ilMatFile.GetArray<double>("wt_cp_table");
-            this.cp.beta = ilMatFile.GetArray<double>("wt_cp_beta");
-            this.cp.tsr = ilMatFile.GetArray<double>("wt_cp_tsr");
-            this.ct.table = ilMatFile.GetArray<double>("wt_ct_table");
-            this.ct.beta = ilMatFile.GetArray<double>("wt_ct_beta");
-            this.ct.tsr = ilMatFile.GetArray<double>("wt_ct_tsr");
+            this.cp.table                   = ilMatFile.GetArray<double>("wt_cp_table");
+            this.cp.beta                    = ilMatFile.GetArray<double>("wt_cp_beta");
+            this.cp.tsr                     = ilMatFile.GetArray<double>("wt_cp_tsr");
+            this.ct.table                   = ilMatFile.GetArray<double>("wt_ct_table");
+            this.ct.beta                    = ilMatFile.GetArray<double>("wt_ct_beta");
+            this.ct.tsr                     = ilMatFile.GetArray<double>("wt_ct_tsr");
+            this.rotor.radius               = (double)(ilMatFile.GetArray<double>("wt_rotor_radius"));
+            this.rotor.inertia              = (double)(ilMatFile.GetArray<double>("wt_rotor_inertia"));
+            this.rotor.ratedspeed           = (double)(ilMatFile.GetArray<double>("wt_rotor_ratedspeed"));
+            this.ctrl.pitch.Pgain           = (double)(ilMatFile.GetArray<double>("wt_ctrl_pitch_Pgain"));
+            this.ctrl.pitch.Igain           = (double)(ilMatFile.GetArray<double>("wt_ctrl_pitch_Igain"));
+            this.ctrl.pitch.ratelim         = (double)(ilMatFile.GetArray<double>("wt_ctrl_pitch_ratelim"));
+            this.ctrl.pitch.ulim            = (double)(ilMatFile.GetArray<double>("wt_ctrl_pitch_ulim"));
+            this.ctrl.pitch.llim            = (double)(ilMatFile.GetArray<double>("wt_ctrl_pitch_llim"));
+            this.ctrl.torq.ratelim          = (double)(ilMatFile.GetArray<double>("wt_ctrl_torq_ratelim"));
+            this.ctrl.p_rated               = (double)(ilMatFile.GetArray<double>("wt_ctrl_p_rated"));
         }
 
     }

6d27b38856be74e97d49e3547a182f9727d2ce32
allchk-nrg <alexey.lychkin@nrg-soft.com>
2015-01-16 08:42:46 +0300

    WFC MatlabInterpretter: added methods

---
+++ Offwind.WakeFarmControl/MatlabInterpreter/ArrayExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatlabInterpreter
+{
+    public static class ArrayExtensions
+    {
+        public static void Fill<T>(this T[] array, T value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = value;
+            }
+        }  
+    }
+}
